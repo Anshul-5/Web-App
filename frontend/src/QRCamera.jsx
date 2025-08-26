46aeef8a4b3bd787ecd06f656e8f3bc9018a74de
Ananyachauhan19 <ANANYACHAUHAN.230125038@gehu.ac.in>
2025-08-26 20:01:43 +0530

    Scanned

--- frontend/src/QRCamera.jsx
+++ frontend/src/QRCamera.jsx
@@ -14,7 +14,7 @@ export default function QRCamera() {
     setStatus('Sending to backend...');
     setScanning(true);
     try {
-      const response = await fetch('http://localhost:5000/api/scan', {
+    const response = await fetch('https://web-app-rpgh.onrender.com/api/scan', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ qrId: qrText }),
