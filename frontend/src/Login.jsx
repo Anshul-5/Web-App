4260ebcef3d6ded273982335a710f7afbdeb45a0
Anubhav-dhyani <adhyani1105@gmail.com>
2025-08-28 18:55:50 +0530

    vbnm,.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,7 +11,7 @@ export default function Login({ onLogin }) {
     setLoading(true);
     setError('');
     try {
-  const res = await fetch('https://web-app-p658.onrender.com/api/login', {
+  const res = await fetch('https://web-app-npyk.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
